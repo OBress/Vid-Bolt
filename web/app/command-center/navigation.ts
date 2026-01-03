bb71cd138b92fc590d986ab571cb94254117d258
OBress <owen@bress.tv>
2026-01-03 11:47:17 -0500

    removes redundant files (niche settings we don't actaully use since everything is stored in the channel settings).

--- web/app/command-center/navigation.ts
+++ web/app/command-center/navigation.ts
@@ -49,11 +49,6 @@ export const NAV_GROUPS: NavGroup[] = [
         label: "General Settings",
         href: "/command-center/settings/general",
       },
-      {
-        id: "niche-manager",
-        label: "Niche Manager",
-        href: "/command-center/settings/niche-manager",
-      },
     ],
   },
 ];
